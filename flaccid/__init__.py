024536a62566b9921f0a72813505a681ffa07990
Phillip Tennen <pt@datatheorem.com>
2017-12-17 18:34:48 -0500

    generic metadata block parsing

--- flaccid/__init__.py
+++ flaccid/__init__.py
@@ -1,24 +1,52 @@
-from ctypes import sizeof
-from bitfield import make_bf, c_uint, c_ulong
+from ctypes import sizeof, Structure, BigEndianStructure, c_uint, c_uint32, c_uint64
+from enum import Enum
 
 
-StreamInfoStruct = make_bf(
-    'StreamInfoStruct',
-    [
-        ('min_block_size', c_uint, 16),
-        ('max_block_size', c_uint, 16),
-        ('min_frame_size', c_uint, 24),
-        ('max_frame_size', c_uint, 24),
-        ('sample_rate_hz', c_uint, 20),
-        ('num_channels', c_uint, 3),
-        ('bits_per_sample', c_uint, 5),
-        #('total_sample_count', c_ulong, 36),
-        #('md5_sig_low', c_uint, 32),
-        #('md5_sig_high', c_uint, 32),
-        #('md5_sig_high1', c_uint, 32),
-        #('md5_sig_high2', c_uint, 32),
+class MetadataBlockType(Enum):
+    STREAMINFO = 0
+    PADDING = 1
+    APPLICATION = 2
+    SEEKTABLE = 3
+    VORBIS_COMMENT = 4
+    CUESHEET = 5
+    PICTURE = 6
+    # types 7 through 126 are reserved
+    INVALID = 127
+
+
+class MetadataBlockHeader(BigEndianStructure):
+    _pack_ = 1
+    _fields_ = [
+        ('is_last_block', c_uint32, 1),
+        ('block_type', c_uint32, 7),
+        ('length', c_uint32, 24),
     ]
-)
+
+
+class MetadataBlockData(BigEndianStructure):
+    _pack_ = 1
+
+
+class MetadataBlockStreamInfo(MetadataBlockData):
+    _fields = [
+        ('min_block_size', c_uint32, 16),
+        ('max_block_size', c_uint32, 16),
+        ('min_frame_size', c_uint32, 24),
+        ('max_frame_size', c_uint32, 24),
+        ('sample_rate_hz', c_uint32, 20),
+        ('num_channels', c_uint32, 3),
+        ('bits_per_sample', c_uint32, 5),
+        ('total_sample_count', c_uint64, 36),
+        ('md5_low', c_uint64, 64),
+        ('md5_high', c_uint64, 64),
+    ]
+
+
+class MetadataBlock(object):
+    def __init__(self, header, data):
+        # type: (MetadataBlockHeader, MetadataBlockData) -> None
+        self.header = header
+        self.data = data
 
 
 class FlacParser(object):
@@ -28,7 +56,16 @@ class FlacParser(object):
         self.parse_magic()
 
         self.stream_info = None
-        self.parse_stream_info()
+
+        # FLAC guarantees at least one metadata block; the stream info block
+        self.metadata_blocks = []
+        while True:
+            metadata_block = self.parse_metadata_block()
+            self.metadata_blocks.append(metadata_block)
+
+            if metadata_block.header.is_last_block:
+                break
+        print('Finished parsing {} metadata blocks'.format(len(self.metadata_blocks)))
 
     def parse_magic(self):
         correct_magic = b'fLaC'
@@ -38,18 +75,34 @@ class FlacParser(object):
                 print('incorrect magic byte {}, expected {}'.format(b, correct_magic[i]))
         print('FLAC magic verified')
 
-    def parse_stream_info(self):
-        stream_info_bytes = bytearray(bytes(self.flac.read(sizeof(StreamInfoStruct))))
-        print(stream_info_bytes)
-        self.stream_info = StreamInfoStruct.from_buffer(stream_info_bytes)
+    def parse_metadata_header(self):
+        header_bytes = bytearray(bytes(self.flac.read(sizeof(MetadataBlockHeader))))
+        header = MetadataBlockHeader.from_buffer(header_bytes)
+        return header
 
-        print('FLAC smallest block size: {}'.format(self.stream_info.min_block_size))
-        print('FLAC biggest block size: {}'.format(self.stream_info.max_block_size))
-        print('FLAC smallest frame size: {}'.format(self.stream_info.min_frame_size))
-        print('FLAC biggest frame size: {}'.format(self.stream_info.max_frame_size))
-        print('FLAC sample rate (in Hz): {}'.format(self.stream_info.sample_rate_hz))
-        print('FLAC channel count: {}'.format(self.stream_info.num_channels))
-        print('FLAC bits per sample: {}'.format(self.stream_info.bits_per_sample))
+    def parse_data_for_metadata_header(self, header):
+        # type: (MetadataBlockHeader) -> MetadataBlockData
+        if header.block_type == MetadataBlockType.STREAMINFO.value:
+            data_type = MetadataBlockStreamInfo
+        else:
+            raise NotImplementedError('block type {}'.format(header.block_type))
+        data_size = sizeof(data_type)
+        data_bytes = bytearray(bytes(self.flac.read(data_size)))
+        data = data_type.from_buffer(data_bytes)
+        return data
+
+    def parse_metadata_block(self):
+        header = self.parse_metadata_header()
+        block_end = self.flac.tell() + header.length
+        print('Parsing {} metadata block ({} bytes)'.format(MetadataBlockType(header.block_type).name, header.length))
+        if header.is_last_block:
+            print('This is the last metadata block before audio blocks')
+        data = self.parse_data_for_metadata_header(header)
+
+        # seek to beginning of next block
+        self.flac.seek(block_end)
+
+        return MetadataBlock(header, data)
 
 file = 'Bombtrack.flac'
 with open(file, 'rb') as flac_file:
